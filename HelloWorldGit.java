96a739cb91169a31b20948777cb9fa6f53ba9f0d
teja_shrivant <teja_shrivant@persistent.co.in>
2018-07-26 15:10:28 +0530

    calling static

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,6 +1,6 @@
 class  HelloWorld{
 
-void displayDate(){
+static void displayDate(){
 
 system.out.println("display today's date:-");
 }
@@ -9,7 +9,7 @@ public static void main(String[] args)
 {
 
 system.out.println("Hello World...welcome to git");
-
+displayGit()
 }
 
 }
